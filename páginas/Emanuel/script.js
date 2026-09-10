70093ac7a264005990b697e1ff69922b30828c57
emanuelPedrozoRosa <emanuelbirds@gmail.com>
2026-09-10 08:31:22 -0300

    feat: excluir "li" de conteúdos na section.topicos

--- páginas/Emanuel/script.js
+++ páginas/Emanuel/script.js
@@ -1,5 +1,8 @@
-//b1 , d2, b3 , a4 , c5
-
+//RESPOSTAS: b1 , d2, b3 , a4 , c5
+//  uma função serve para armazenar e repetir certas linhas de
+//comando várias vezes, tal qual uma variável.
+//  essa function, serve para passar as perguntas ao usuário,
+//no caso as entradas do código.
 function validarResposta(elemento, pergunta) {
     console.log(pergunta)
     console.log(elemento)
@@ -11,4 +14,10 @@ function validarResposta(elemento, pergunta) {
         
     }
 }
+//preciso pesquisar uma função para o processamento.
+//meu ideal seria: resposta escolhida --> verifica pelo ID do elemento --\/
+//--> esse ID vai ter o valor 1 para certo e 0 para errado, que vai ser incrementando em outra let --\/
+//--> o número de acertos será verificado em um ninho de IFs, com diferentes reações em texto --\/
+//--> apresenta o percentual de acertos e o texto de reação.
+
 
